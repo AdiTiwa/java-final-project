9321435e07fb16733cd29ffebd79888e30dd6636
Aditya Tiwari <67660542+AdiTiwa@users.noreply.github.com>
2022-12-16 15:42:20 -0600

    day 4: diologue works lets go

--- Day1Start_Movement_with_Inputs.java
+++ Day1Start_Movement_with_Inputs.java
@@ -56,6 +56,9 @@ public class Day1Start_Movement_with_Inputs {
         //keeps track of mouse coordinates
         int[] mouse = {0,0};
         
+        boolean inDiologue = false;
+        boolean inInventory = true;
+        
         public class Transition {
             int[] transRect;
             int[] reset;
@@ -161,45 +164,59 @@ public class Day1Start_Movement_with_Inputs {
             }
         }
         
-        public class Projectile {
+        public class NPC {
+            String[] diologue;
+            String name;
+            Animation NPCanimation;
+            int currentDiologue;
+            int[] position;
+            int interactionTimer = 0;
             boolean active;
-            int[] rect;
-            int[] velocity;
             
-            public Projectile(int[] r, int[] v) {
-                this.rect = r;
-                this.velocity = v;
+            public NPC (Animation NPC, String n, int[] pos, String[] dlque) {
+                this.diologue = dlque;
+                this.name = n;
+                this.position = pos;
+                this.NPCanimation = NPC;
                 this.active = true;
+                
             }
             
-            public void update() {
-                for (int[] wall : levels[currentLevel]) {
-                    if (Methods.collided(this.rect, wall, this.velocity)) {
-                        this.active = false;
-                        
-                        return;
-                    }
+            public boolean questbegin() {
+                return false;
+            }
+            
+            public void renderNPC(Graphics g) {
+                this.NPCanimation.drawImage(this.position, g);
+                this.NPCanimation.nextFrame();
+            }
+            
+            public String getCurrentDiologue() {
+                if (currentLine > this.getMaxNPCDiologue() - 1) {
+                    inDiologue = false;
+                    currentNPC = null;
+                    currentLine = 0;
                 }
                 
-                this.rect[0] += this.velocity[0];
-                this.rect[1] += this.velocity[1];
-            } 
-        }
-        
-        public class ProjectileSpawner {
-            int rockTimer = 0;
-            int rockDelay;
-            int[] spawnPosition;
-            int[] velocity;
-            
-            
-            public ProjectileSpawner (int rDelay, int[] position, int[] rockVelocity) {
-                this.rockDelay = rockDelay;
-                this.spawnPosition = position;
-                this.velocity = rockVelocity;
+                return this.diologue[currentLine];
             }
             
+            public String getNPCName() {
+                return this.name;
+            }
             
+            public int getMaxNPCDiologue() {
+                return diologue.length;
+            }
+            
+            public void updateNPC() {
+                if (Methods.collided(playerPos, this.position, playerVel) && this.active) {
+                    playerVel = new int[]{-1 * playerVel[0], -1 * playerVel[1]};
+                    
+                    inDiologue = true;
+                    currentNPC = this;
+                }
+            }
         }
 
         
@@ -221,6 +238,19 @@ public class Day1Start_Movement_with_Inputs {
             put(1, new PickUp[]{});
         }};
         
+        HashMap<Integer, NPC[]> npcs = new HashMap<>() {{
+            put(0, new NPC[] {});
+            put(1, new NPC[] {
+                new NPC(new Animation (new String[]{"npc olivia1.png", "npc olivia2.png"}, 2, 20), "Person", new int[]{250, 250, 32, 48}, new String[] {
+                    "Hello world!",
+                    "Say Hi!",
+                }),
+            });
+        }};
+        
+        int currentLine = 0;
+        NPC currentNPC;
+        
         HashMap<String, Integer> inventory = new HashMap<>() {{
             put("Pictures", 0);
         }};
@@ -228,8 +258,12 @@ public class Day1Start_Movement_with_Inputs {
         Image[] pictures = new Image[]{
             Toolkit.getDefaultToolkit().getImage("picture frame.png"),
             Toolkit.getDefaultToolkit().getImage("picture2.png"),
-            
+            Toolkit.getDefaultToolkit().getImage("picture 3.png"),
+            Toolkit.getDefaultToolkit().getImage("picture 4.png"),
+            Toolkit.getDefaultToolkit().getImage("picture 5.png"),
         };
+        
+        int currentNPCtime = 0;
         
         
         public void paintComponent(Graphics g) {
@@ -254,10 +288,10 @@ public class Day1Start_Movement_with_Inputs {
             if (currentLevel < 0) {
                 g.setColor(new Color(0, 0, 0));
                 g.fillRect(0, 0, 500, 500);
-                g.setFont(new Font("Dialog", Font.PLAIN, 18));
+                g.setFont(new Font("Dialog", Font.PLAIN, 32));
                 g.setColor(new Color(100, 100, 100));
                 g.drawString("poenitire", 100, 100);
-                g.drawString("press space to start", 100, 400);
+                g.drawString("press space to start", 100, 400); 
                 
                 if (inputs.get("space")) {
                     currentLevel = 0;
@@ -271,13 +305,27 @@ public class Day1Start_Movement_with_Inputs {
                 drawWalls(g);
                 drawPickups(g);
                 
+                for (NPC npc : npcs.get(currentLevel)) {
+                    npc.updateNPC();
+                    npc.renderNPC(g);
+                }
+                
                 if (inputs.get("K")) {
+                    inInventory = true;
                     drawInvHUD(g);
+                }
+                
+                if (inInventory && !inputs.get("K")) {
+                    inInventory = false;
+                }
+                
+                if (inDiologue) {
+                    drawDiologue(g);
                 }
             }
             
             //draws the HUD
-            Methods.HUD(g, mouse, inputs);
+            // Methods.HUD(g, mouse, inputs);
         }
         
         /*
@@ -288,52 +336,52 @@ public class Day1Start_Movement_with_Inputs {
             // 2) see if with that velocity, player would hit a wall
             // 3) if so, make player velocity 0 in that direction
             // 4) move player based on new velocity
-            
-            if (inputs.get("up")) {
-                playerVel[1] = -playerSpeed;
-            } else if (inputs.get("down")) {
-                playerVel[1] = playerSpeed;
-            } else {
-                playerVel[1] = 0;
-            }
-            
-            if (inputs.get("left")) {
-                playerVel[0] = -playerSpeed;
-                player.setFlipped(true);
-            } else if (inputs.get("right")) {
-                playerVel[0] = playerSpeed;
-                player.setFlipped(false);
-            } else {
-                playerVel[0] = 0;
-            }
-            
-            for (int[] wall : levels[currentLevel]) {
-                if (Methods.collided(playerPos, wall, playerVel)) {
-                    playerVel[1] -= playerVel[1];
-                    playerVel[0] -= playerVel[0];
+            if (!(inInventory || inDiologue)) {
+                if (inputs.get("up")) {
+                    playerVel[1] = -playerSpeed;
+                } else if (inputs.get("down")) {
+                    playerVel[1] = playerSpeed;
+                } else {
+                    playerVel[1] = 0;
                 }
-            }
-            
-            for (Transition trans : transitions.get(currentLevel)) {
-                if (Methods.collided(playerPos, trans.getRect(), playerVel)) {
-                    currentLevel = trans.getNextRoom();
-                    playerPos[0] = trans.getReset()[0];
-                    playerPos[1] = trans.getReset()[1];
+                
+                if (inputs.get("left")) {
+                    playerVel[0] = -playerSpeed;
+                    player.setFlipped(true);
+                } else if (inputs.get("right")) {
+                    playerVel[0] = playerSpeed;
+                    player.setFlipped(false);
+                } else {
+                    playerVel[0] = 0;
                 }
-            }
-            
-            for (PickUp pickup : pickups.get(currentLevel)) {
-                if (Methods.collided(playerPos, pickup.getRect(), playerVel) && pickup.isActive() && inputs.get("J")) {
-                    pickup.notActive();
-                    inventory.put("Pictures", inventory.get("Pictures") + 1);
+                
+                for (int[] wall : levels[currentLevel]) {
+                    if (Methods.collided(playerPos, wall, playerVel)) {
+                        playerVel[1] -= playerVel[1];
+                        playerVel[0] -= playerVel[0];
+                    }
                 }
+                
+                for (Transition trans : transitions.get(currentLevel)) {
+                    if (Methods.collided(playerPos, trans.getRect(), playerVel)) {
+                        currentLevel = trans.getNextRoom();
+                        playerPos[0] = trans.getReset()[0];
+                        playerPos[1] = trans.getReset()[1];
+                    }
+                }
+                
+                for (PickUp pickup : pickups.get(currentLevel)) {
+                    if (Methods.collided(playerPos, pickup.getRect(), playerVel) && pickup.isActive() && inputs.get("J")) {
+                        pickup.notActive();
+                        inventory.put("Pictures", inventory.get("Pictures") + 1);
+                    }
+                }
+                
+                playerPos[1] += playerVel[1];
+                playerPos[0] += playerVel[0];
+                
+                player.nextFrame();
             }
-            
-            playerPos[1] += playerVel[1];
-            playerPos[0] += playerVel[0];
-            
-            player.nextFrame();
-
         }
         
         /*
@@ -379,7 +427,20 @@ public class Day1Start_Movement_with_Inputs {
         public void drawDiologue (Graphics g) {
             g.setColor(new Color(50, 50, 50));
             g.fillRect(100, 400, 300, 100);
+            g.setFont(new Font("Display", Font.PLAIN, 16));
             
+            currentNPCtime++;
+            boolean canNext = currentNPCtime > 100;
+            if (canNext)
+                currentNPCtime = 0;
+            
+            if (inputs.get("J") && canNext) {
+                currentLine++;
+            }
+            
+            g.setColor(new Color(200, 200, 200));
+            g.drawString(currentNPC.getNPCName(), 125, 415);
+            g.drawString(currentNPC.getCurrentDiologue(), 125, 450);
         }
         
         /*********************************************************
@@ -484,7 +545,7 @@ public class Day1Start_Movement_with_Inputs {
     private void go() {
         frame = new JFrame("Final Project");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500, 640));
+        frame.setPreferredSize(new Dimension(500, 515));
         frame.setBackground(new Color(255, 255, 255));
         frame.pack();
         drawPanel = new DrawPanel();
